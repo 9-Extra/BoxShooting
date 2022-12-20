acf1d2bbb9d576dd9c373e6860d59b138b5c6841
9-Extra <77313011@qq.com>
2022-12-21 03:44:41 +0800

    将图像api改为d3d12

--- Display.cpp
+++ Display.cpp
@@ -55,8 +55,7 @@ static LRESULT CALLBACK WindowProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lP
 	return true;
 }
 
-Display::Display(unsigned int width, unsigned int height)
-	:width(width), height(height)
+void Display::init_window()
 {
 	HINSTANCE hInstance = GetModuleHandleW(NULL);
 
@@ -83,7 +82,7 @@ Display::Display(unsigned int width, unsigned int height)
 		.bottom = 100 + (LONG)height
 	};
 	AdjustWindowRectEx(&rect, WINDOW_STYLE, NULL, 0);
-	
+
 	hwnd = CreateWindowExW(
 		0,
 		L"MyWindow",
@@ -102,71 +101,223 @@ Display::Display(unsigned int width, unsigned int height)
 	if (hwnd == NULL) {
 		SysError(NULL);
 	}
+}
 
-	HRESULT hr;
-
-	const DXGI_SWAP_CHAIN_DESC scd = {
-		.BufferDesc = DXGI_MODE_DESC{
-			.Width = width,
-			.Height = height,
-			.RefreshRate = DXGI_RATIONAL{.Numerator = 0u, .Denominator = 0u},
-			.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM,
-			.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER::DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED,
-			.Scaling = DXGI_MODE_SCALING::DXGI_MODE_SCALING_CENTERED,
-		},
-		.SampleDesc = DXGI_SAMPLE_DESC{
-			.Count = 1,
-			.Quality = 0
-		},
-		.BufferUsage = DXGI_USAGE_BACK_BUFFER | DXGI_USAGE_RENDER_TARGET_OUTPUT,
-		.BufferCount = 2,
-		.OutputWindow = hwnd,
-		.Windowed = TRUE,
-		.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL,
-		.Flags = 0
-	};
-
-	UINT device_flag = 0;
+void Display::init_pipeline()
+{
 #ifndef NDEBUG
-	device_flag = D3D11_CREATE_DEVICE_FLAG::D3D11_CREATE_DEVICE_DEBUG;
-#endif // !NDEBUG
+	// Enable the D3D12 debug layer.
+	{
+		winrt::com_ptr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+		{
+			debugController->EnableDebugLayer();
+		}
+	}
+#endif
 
-	hr = D3D11CreateDeviceAndSwapChain(
-		NULL,
-		D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_HARDWARE,
-		nullptr,
-		device_flag,
-		nullptr,
-		0,
-		D3D11_SDK_VERSION,
-		&scd,
-		p_swapchain.put(),
-		p_device.put(),
-		NULL,
-		p_context.put()
+	winrt::com_ptr<IDXGIFactory4> factory;
+	CreateDXGIFactory1(IID_PPV_ARGS(&factory));
+
+	
+	winrt::com_ptr<IDXGIAdapter1> hardwareAdapter;
+	factory->EnumAdapters1(0, hardwareAdapter.put());
+	DXGI_ADAPTER_DESC adapter_desc;
+	hardwareAdapter->GetDesc(&adapter_desc);
+	debug_log("%ws\n", adapter_desc.Description);
+
+	D3D12CreateDevice(
+		hardwareAdapter.get(),
+		D3D_FEATURE_LEVEL_12_0,
+		IID_PPV_ARGS(p_device.put())
+	);
+	
+
+	// Describe and create the command queue.
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	p_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(p_command_queue.put()));
+
+	// Describe and create the swap chain.
+	DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+	swapChainDesc.BufferCount = FrameCount;
+	swapChainDesc.BufferDesc.Width = width;
+	swapChainDesc.BufferDesc.Height = height;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+	swapChainDesc.OutputWindow = hwnd;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.Windowed = TRUE;
+
+	winrt::com_ptr<IDXGISwapChain> swapChain;
+	factory->CreateSwapChain(
+		p_command_queue.get(),        // Swap chain needs the queue so that it can force a flush on it.
+		&swapChainDesc,
+		swapChain.put()
 	);
 
-	if (FAILED(hr)) {
-		SysError(L"Failed to create device");
+	swapChain.as(p_swapchain);
+
+	// This sample does not support fullscreen transitions.
+	factory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_ALT_ENTER);
+
+	//unsigned int m_frameIndex = p_swapchain->GetCurrentBackBufferIndex();
+
+	// Create descriptor heaps.
+	{
+		// Describe and create a render target view (RTV) descriptor heap.
+		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+		rtvHeapDesc.NumDescriptors = FrameCount;
+		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		p_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(p_rtvHeap.put()));
+
+		rtvDescriptorSize = p_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	}
+
+	// Create frame resources.
+	{
+		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(p_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+		// Create a RTV for each frame.
+		for (UINT n = 0; n < FrameCount; n++)
+		{
+			p_swapchain->GetBuffer(n, IID_PPV_ARGS(rendertargets[n].put()));
+			p_device->CreateRenderTargetView(rendertargets[n].get(), nullptr, rtvHandle);
+			rtvHandle.Offset(1, rtvDescriptorSize);
+		}
+	}
+
+	p_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&p_command_allocator));
+}
+
+void Display::init_assests()
+{
+	// Create an empty root signature.
+	{
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		winrt::com_ptr<ID3DBlob> signature;
+		winrt::com_ptr<ID3DBlob> error;
+		D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, signature.put(), error.put());
+		p_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(p_root_signature.put()));
+	}
+	
+	p_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, p_command_allocator.get(), NULL, IID_PPV_ARGS(p_command_list.put()));
+	p_command_list->Close();
+
+	p_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(p_fence.put()));
+	fence_value = 1;
+	fence_event = CreateEventW(nullptr, FALSE, FALSE, nullptr);
+
+	CD3DX12_HEAP_PROPERTIES heap_desc(D3D12_HEAP_TYPE_UPLOAD);
+	CD3DX12_RESOURCE_DESC resource_desc = CD3DX12_RESOURCE_DESC::Buffer(width * height * sizeof(Color));
+	p_device->CreateCommittedResource(&heap_desc, D3D12_HEAP_FLAG_CREATE_NOT_ZEROED, &resource_desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&p_upload_buffer));
+}
+
+void Display::populate_command_list()
+{
+	// Command list allocators can only be reset when the associated 
+	// command lists have finished execution on the GPU; apps should use 
+	// fences to determine GPU execution progress.
+	p_command_allocator->Reset();
+
+	// However, when ExecuteCommandList() is called on a particular command 
+	// list, that command list can then be reset at any time and must be before 
+	// re-recording.
+	p_command_list->Reset(p_command_allocator.get(), NULL);
+
+
+	// Indicate that the back buffer will be used as a render target.
+	unsigned int frame_index = p_swapchain->GetCurrentBackBufferIndex();
+
+	auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(rendertargets[frame_index].get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_COPY_DEST);
+	p_command_list->ResourceBarrier(1, &barrier);
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(p_rtvHeap->GetCPUDescriptorHandleForHeapStart(), frame_index, rtvDescriptorSize);
+	p_command_list->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+
+	ID3D12Resource* back_buffer;
+	if (FAILED(p_swapchain->GetBuffer(frame_index, IID_PPV_ARGS(&back_buffer)))) {
+		SysError(NULL);
+	}
+
+	CD3DX12_TEXTURE_COPY_LOCATION dst_location(back_buffer, 0);
+	D3D12_TEXTURE_COPY_LOCATION src_location = {
+		.pResource = p_upload_buffer.get(),
+		.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT,
+		.PlacedFootprint = {
+			.Offset = 0,
+			.Footprint = {
+				.Format = DXGI_FORMAT_R8G8B8A8_UINT,
+				.Width = width,
+				.Height = height,
+				.Depth = 1u,
+				.RowPitch = width * sizeof(Color)
+			}
+		}
+	};
+
+	p_command_list->CopyTextureRegion(&dst_location, 0, 0, 0, &src_location, nullptr);
+
+	back_buffer->Release();
+
+	// Record commands.
+	//const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	//p_command_list->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+
+	// Indicate that the back buffer will now be used to present.
+	barrier = CD3DX12_RESOURCE_BARRIER::Transition(rendertargets[frame_index].get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PRESENT);
+	p_command_list->ResourceBarrier(1, &barrier);
+
+	p_command_list->Close();
+
+}
+
+void Display::wait_till_next_frame()
+{
+	unsigned int fence = fence_value++;
+	p_command_queue->Signal(p_fence.get(), fence);
+
+	if (p_fence->GetCompletedValue() < fence) {
+		p_fence->SetEventOnCompletion(fence, fence_event);
+		WaitForSingleObject(fence_event, INFINITE);
+	}
+}
+
+Display::Display(unsigned int width, unsigned int height)
+	:width(width), height(height)
+{
+
+	init_window();
+	init_pipeline();
+	init_assests();
+	wait_till_next_frame();
 
 	ShowCursor(false);
 	ShowWindow(hwnd, SW_SHOW);
 }
 
 void Display::swap(Color* panel) {
+	Color* data;
+	p_upload_buffer->Map(0, nullptr, (void**) & data);
+	memcpy(data, panel, width * height * sizeof(Color));
+	p_upload_buffer->Unmap(0, nullptr);
 
+	populate_command_list();
 
-	ID3D11Texture2D* back_buffer;
-	if (FAILED(p_swapchain->GetBuffer(0, IID_PPV_ARGS(&back_buffer)))) {
-		SysError(NULL);
-	}
-	
-	p_context->UpdateSubresource(back_buffer, 0, NULL, panel, WINDOW_WIDTH * sizeof(Color), WINDOW_HEIGHT * sizeof(Color));
+	ID3D12CommandList* ppCommandLists[] = { p_command_list.get() };
+	p_command_queue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 
 	if (FAILED(p_swapchain->Present(1, 0))) {
 		SysError(NULL);
 	};
 
-	back_buffer->Release();
+	wait_till_next_frame();
 }
--- Display.h
+++ Display.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "directx/d3dx12.h"
 #include <d3d11.h>
 #include <dxgi1_6.h>
 #include <winrt/base.h>
@@ -9,16 +10,43 @@
 
 class Display {
 private:
+	static const unsigned int FrameCount = 2;
+
 	HWND hwnd;
-	winrt::com_ptr<ID3D11Device> p_device;
-	winrt::com_ptr<ID3D11DeviceContext> p_context;
-	winrt::com_ptr<IDXGISwapChain> p_swapchain;
+	winrt::com_ptr<ID3D12Device> p_device;
+	winrt::com_ptr<ID3D12CommandQueue> p_command_queue;
+	winrt::com_ptr<ID3D12CommandAllocator> p_command_allocator;
+	winrt::com_ptr<ID3D12DescriptorHeap> p_rtvHeap;
+	winrt::com_ptr<IDXGISwapChain4> p_swapchain;
+
+	winrt::com_ptr<ID3D12RootSignature> p_root_signature;
+	winrt::com_ptr<ID3D12GraphicsCommandList> p_command_list;
+	winrt::com_ptr<ID3D12Fence1> p_fence;
+
+	winrt::com_ptr<ID3D12Resource> p_upload_buffer;
+
+	winrt::com_ptr<ID3D12Resource> rendertargets[FrameCount];
+
+	unsigned int rtvDescriptorSize;
+
+	unsigned int fence_value;
+	HANDLE fence_event;
 
 	unsigned int width;
 	unsigned int height;
 
+	void init_window();
+	void init_pipeline();
+	void init_assests();
+	void populate_command_list();
+
+	void wait_till_next_frame();
+
 public:
 	Display(unsigned int width, unsigned int height);
+	~Display() {
+		wait_till_next_frame();
+	}
 
 	void swap(Color* panel);
 
